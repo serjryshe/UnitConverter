afd6d57a184e650e277a3c2a6ecdbe48de4b2785
serjryshe <serj.ryshe@gmail.com>
2022-08-15 23:10:10 -0700

    1

---
+++ index.js
@@ -0,0 +1,29 @@
+const convertBtn = document.getElementById("convert-btn")
+const lengthEl = document.getElementById("length-el")
+const volumeEl = document.getElementById("volume-el")
+const massEl = document.getElementById("mass-el")
+
+
+
+convertBtn.addEventListener("click", function() {
+    let inputValue = document.querySelector('input').value
+
+
+    let feetValue = inputValue * 3.281
+    let meterValue = inputValue / 3.281
+    let volumeValue = inputValue / 3.785
+    let gallonsValue = inputValue * 3.705
+    let massValue = inputValue * 2.204
+    let poundsValue = inputValue / 2.204
+
+    let lengthMsg = `${inputValue} meters = ${feetValue.toFixed(3)} feet | ${inputValue} feet = ${meterValue.toFixed(3)} meters`
+    lengthEl.innerHTML = lengthMsg
+    
+    let volumeMsg = `${inputValue} liters = ${volumeValue.toFixed(3)} gallons | ${inputValue} gallons = ${gallonsValue.toFixed(3)} liters`
+    volumeEl.innerHTML = volumeMsg
+    
+    let massMsg = `${inputValue} kilos = ${poundsValue.toFixed(3)} pounds | ${inputValue} pounds = ${massValue.toFixed(3)} kilos`
+    massEl.innerHTML = massMsg
+
+})
+
